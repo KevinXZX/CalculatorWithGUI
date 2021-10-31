fffabdee57581b69384d53acdb0789334d879a86
KevinXZX <72686328+KevinXZX@users.noreply.github.com>
2021-10-31 15:53:34 +0000

    Changed font size and panel size

--- src/GUI/Display.java
+++ src/GUI/Display.java
@@ -8,13 +8,15 @@ public class Display extends JPanel {
     JLabel label;
     String sum = "";
     public Display(){
+        Font f = new Font("serif", Font.BOLD,25);
         label = new JLabel();
         this.setBackground(Color.WHITE);
-        this.setBounds(50,0,600,75);
+        this.setBounds(25,25,475,75);
         label.setForeground(Color.BLACK);
+        label.setFont(f);
+        this.add(label);
     }
     public void update(String input){
-        int result = 0;
         if(input.equals("=")){
             sum = Double.toString(calculate(sum));
         }
@@ -22,12 +24,12 @@ public class Display extends JPanel {
             sum = sum + input;
         }
         label.setText(sum);
+        this.add(label);
     }
     public double calculate(String x){
-        ArrayList<Double> numbers = new ArrayList<Double>();
-        ArrayList<Character> signs = new ArrayList<Character>();
+        ArrayList<Double> numbers = new ArrayList<>();
+        ArrayList<Character> signs = new ArrayList<>();
         int startIndex = 0;
-        int result = 0;
         for(int i = 0;i<x.length();i++){
             char c = x.charAt(i);
             if(!(Character.isDigit(c)) && c != '.'){
